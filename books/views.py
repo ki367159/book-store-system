2b1726facc5c8df68224331124dffdde2896a901
kiki <ki367159@gmail.com>
2019-01-02 21:13:01 +0800

    0102

--- books/views.py
+++ books/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 from utils.forms import DeleteConfirmForm
 from .forms import BookForm
 from .models import Book
 
-
+@login_required
 def index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
 
-
+@login_required
 def show(request, pk):
     # try:
     #     book = Book.objects.get(pk=pk)
@@ -19,7 +20,8 @@ def show(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'books/show.html', {'book': book})
 
-
+@login_required
+@permission_required('books.add_book', login_url='permission_denied')
 def add(request):
     form = BookForm(request.POST or None)
     if form.is_valid():
@@ -29,7 +31,7 @@ def add(request):
 
     return render(request, 'books/add.html', {'form': form})
 
-
+@login_required
 def edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     form = BookForm(request.POST or None, instance=book)
@@ -40,7 +42,7 @@ def edit(request, pk):
 
     return render(request, 'books/edit.html', {'form': form})
 
-
+@login_required
 def delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
     form = DeleteConfirmForm(request.POST or None)
